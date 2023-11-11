8dd18b7352c55d9ce3ed06231d96a540c3dbdb7a
Lucas Trindade <trindadebra@gmail.com>
2023-11-11 19:26:51 -0300

    Feat: Scroll Behavior no HTML

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR" className='scroll-smooth'>
       <Head />
       <body>
         <Main />
