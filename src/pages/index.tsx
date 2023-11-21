ce3a138c566ba04fa0dfbe603e0de3479855fd2b
Lucas Trindade <trindadebra@gmail.com>
2023-11-21 17:19:53 -0300

    Revalidate update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,6 @@ export const getStaticProps: GetStaticProps = async () => {
       certificateData,
       experienceData
     },
-    revalidate: 60 * 30, //30min
+    revalidate: 60 * 60 * 24, //24h
   };
 };
