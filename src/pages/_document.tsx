630d50c8c4fcc8766225f787e2b64fe8e57dea0e
Lucas Trindade <trindadebra@gmail.com>
2023-11-23 14:46:37 -0300

    @vercel/analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -7,6 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   )
