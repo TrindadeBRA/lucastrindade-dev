b5aa3697624e6832785b6cb8c2024971821b6438
Lucas Trindade <lucas.trindade@uds.com.br>
2024-10-11 11:35:16 -0300

    profile image

--- src/pages/api/sectionProfile.ts
+++ src/pages/api/sectionProfile.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from '@notionhq/client';
 
+import { downloadImage, extractFileName } from '@/utils/Utils';
+
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 export interface Profile {
@@ -16,14 +18,28 @@ export async function getSectionProfile(): Promise<Profile> {
   const response = await notion.databases.query({
     database_id: "18c52f2be7fa439c8049679e724ae7f3",
   });
-    
-  const mappedResult = response.results.map((user: any) => ({
-    user_name: user.properties['user_name'].title[0].text.content,
-    user_role: user.properties['user_role'].rich_text[0].text.content,
-    user_bio: user.properties['user_bio'].rich_text[0].text.content,
-    user_avatar: user.properties['user_avatar'].files[0]?.file?.url,
-    user_avatar_url: user.properties['user_avatar_url'].url,
-    user_presentation: user.properties['user_presentation'].rich_text,
+
+  const mappedResult = await Promise.all(response.results.map(async (user: any) => {
+    const userName = user.properties['user_name'].title[0].text.content;
+    const userRole = user.properties['user_role'].rich_text[0].text.content;
+    const userBio = user.properties['user_bio'].rich_text[0].text.content;
+    const avatarUrl = user.properties['user_avatar'].files[0]?.file?.url;
+
+    let localAvatarPath = '';
+    if (avatarUrl) {
+      const sanitizedFileName = extractFileName(avatarUrl);
+      const filename = `${userName.replace(/\s+/g, '_').toLowerCase()}_${sanitizedFileName}`;
+      localAvatarPath = await downloadImage(avatarUrl, filename, './public/images/avatars');
+    }
+
+    return {
+      user_name: userName,
+      user_role: userRole,
+      user_bio: userBio,
+      user_avatar: localAvatarPath,
+      user_avatar_url: avatarUrl,
+      user_presentation: user.properties['user_presentation'].rich_text,
+    };
   }));
 
   return mappedResult[0];
