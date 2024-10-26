3229f703f771c3dbd774b04fdccdd24deae394b0
Lucas Trindade <trindadebra@gmail.com>
2024-10-26 14:38:51 -0300

    feat: carrosel added

--- src/components/Certificates/index.tsx
+++ src/components/Certificates/index.tsx
@@ -2,6 +2,14 @@ import { Certificate } from '@/pages/api/sectionCertificates';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 export default function Certificates(certificateData: Certificate[]) {
 
   const [showModal, setShowModal] = useState(false);
@@ -19,13 +27,44 @@ export default function Certificates(certificateData: Certificate[]) {
   };
 
   return (
-    <div className="bg-gray-900 py-14 sm:py-20" id="certificados">
+    <div className="bg-gradient-to-b from-gray-800 to-gray-900  py-14 sm:py-20" id="certificados">
+
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">Estudos</h2>
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Certificados</h2>
         </div>
-        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+
+        <div className='mt-16'>
+          <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={3}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 1,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            loop={true}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+            autoplay={true}
+          >
+            {Object.values(certificateData).map((certificate: Certificate) => (
+              <SwiperSlide key={certificate.certificate_id}>
+                <Image width={1024} height={720} src={certificate.certificate_file_url} alt={''} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+
+
+        {/* <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           {Object.values(certificateData).map((certificate: Certificate) => (
             <article key={certificate.certificate_id} className="flex flex-col items-start justify-between cursor-pointer bg-gray-800 hover:bg-gray-700 py-5 px-5 rounded-3xl shadow-md shadow-gray-600" onClick={() => openModal(certificate.certificate_file)}>
               <div className="relative w-full cursor-pointer mb-5">
@@ -53,7 +92,7 @@ export default function Certificates(certificateData: Certificate[]) {
               </div>
             </article>
           ))}
-        </div>
+        </div> */}
       </div>
 
       {showModal && (
