7c9d07ba193868506c760f6a307ed7da4d64cc81
Lucas Trindade <trindadebra@gmail.com>
2025-02-26 14:08:20 -0300

    feat: certificate file added

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -21,12 +21,11 @@ export default function Resume({ profileData, skillsData, certificateData, exper
   };
 
 
-  console.log(profileData?.user_presentation?.[0]?.plain_text);
+  console.log(certificateData);
 
   return (
     <>
-      <div id="resume-content" className="bg-white text-black flex flex-col py-4 gap-y-8">
-
+      <div id="resume-content" className="bg-white text-black flex flex-col pt-4 pb-12 gap-y-8">
         {/* Header */}
         <div id="header" className="flex justify-between items-center container mx-auto">
           <div className="w-3/5 text-left flex flex-col">
@@ -60,18 +59,18 @@ export default function Resume({ profileData, skillsData, certificateData, exper
         {/* Presentation */}
         <div id="presentation" className="flex justify-between items-center container mx-auto">
           <div className="text-left flex flex-col">
-            <h2 className="text-xl font-bold mb-4">Apresentação</h2>
+            <Link href="/#apresentacao" target="_blank" className="text-xl font-bold mb-4">Apresentação</Link>
             <div className="flex flex-col gap-y-2">
-                {
-                  profileData?.user_presentation?.[0]?.plain_text
-                    ?.split('\n')
-                    .filter((paragraph: string) => paragraph.trim() !== '')
-                    .map((paragraph: string, index: number) => (
-                      <p key={index} className="text-sm">
-                        {paragraph}
-                      </p>
-                    ))
-                }
+              {
+                profileData?.user_presentation?.[0]?.plain_text
+                  ?.split('\n')
+                  .filter((paragraph: string) => paragraph.trim() !== '')
+                  .map((paragraph: string, index: number) => (
+                    <p key={index} className="text-sm">
+                      {paragraph}
+                    </p>
+                  ))
+              }
             </div>
           </div>
         </div>
@@ -79,7 +78,7 @@ export default function Resume({ profileData, skillsData, certificateData, exper
         {/* Experience */}
         <div id="experience" className="flex justify-between items-center container mx-auto">
           <div className="text-left flex flex-col">
-            <h2 className="text-xl font-bold mb-4">Experiências</h2>
+            <Link href="/#experiencias" target="_blank" className="text-xl font-bold mb-4">Experiências</Link>
             <div className="flex flex-col gap-y-6">
               {experienceData.map((experience) => (
                 <div key={experience.experience_id}>
@@ -98,6 +97,78 @@ export default function Resume({ profileData, skillsData, certificateData, exper
           </div>
         </div>
 
+        {/* Academic Education */}
+        <div id="education" className="flex justify-between items-center container mx-auto">
+          <div className="text-left flex flex-col w-full">
+            <Link href="/#certificados" target="_blank" className="text-xl font-bold mb-4">Formação Acadêmica</Link>
+            <div className="flex flex-col">
+              {certificateData
+                .filter(cert => cert.certificate_category === "Tecnólogo")
+                .map((certificate: Certificate) => (
+                  <div key={certificate.certificate_id} className="flex flex-col mb-2">
+                    <div className="flex items-center gap-2">
+                      <h3 className="text-base font-bold">{certificate.certificate_name}</h3>
+                      {certificate.certificate_instructors && (
+                        <>
+                          <span>-</span>
+                          <p className="text-sm">{certificate.certificate_instructors}</p>
+                        </>
+                      )}
+                    </div>
+                    <p className="text-xs">
+                      {new Date(`${certificate.certificate_date}T00:00:00`).toLocaleDateString('pt-BR')}
+                    </p>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Courses */}
+        <div id="courses" className="flex justify-between items-center container mx-auto">
+          <div className="text-left flex flex-col w-full">
+            <Link href="/#certificados" target="_blank" className="text-xl font-bold mb-4">Cursos e Certificações</Link>
+            <div className="columns-3 gap-6">
+              {certificateData
+                .filter(cert => cert.certificate_category !== "Tecnólogo")
+                .map((certificate: Certificate) => (
+                  <div key={certificate.certificate_id} className="break-inside-avoid-column mb-1 text-sm border-b border-gray-200 pb-1">
+                    <span className="font-medium">{certificate.certificate_name}</span>
+                    <div className="flex justify-between items-center text-xs text-gray-500">
+                      {certificate.certificate_instructors && (
+                        <span>{certificate.certificate_instructors}</span>
+                      )}
+                      <span>
+                        {new Date(`${certificate.certificate_date}T00:00:00`).toLocaleDateString('pt-BR')}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Skills */}
+        <div id="skills" className="flex justify-between items-center container mx-auto">
+          <div className="text-left flex flex-col w-full">
+            <Link href="/#skills" target="_blank" className="text-xl font-bold">Habilidades</Link>
+            <p className="text-sm mb-4">Tecnologias, ferramentas e frameworks com os quais possuo experiência profissional ou conhecimento através de estudos e projetos</p>
+            <div className="rounded-lg">
+              <div className="flex flex-wrap gap-2 justify-around">
+                {Object.values(skillsData)
+                  .sort((a: any, b: any) => a.skill_name.localeCompare(b.skill_name))
+                  .map((skill: any, index) => (
+                    <div
+                      key={index}
+                      className="inline-flex items-center justify-between bg-gray-100 px-3 py-1 rounded-full"
+                    >
+                      <span className="text-sm font-medium">{skill.skill_name}</span>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <button
