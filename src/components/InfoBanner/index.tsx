53ac9b2e06a268d3dc9dcc5c4daffdabb1389f13
Lucas Trindade <trindadebra@gmail.com>
2025-02-26 20:47:57 -0300

    fix: BannerInfo text center

--- src/components/InfoBanner/index.tsx
+++ src/components/InfoBanner/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function InfoBanner() {
   return (
     <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gradient-to-r from-gray-950 to-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 border-y-2 border-gray-800">
-      <p className="text-sm/6 text-white">
+      <p className="text-sm/6 text-white text-center">
         Prefere uma versão para impressão do meu currículo?{' '}
         <Link href="/resume" target="_blank" className="ml-4 whitespace-nowrap font-semibold text-indigo-600 hover:text-indigo-700">
           Baixar versão PDF&nbsp;<span aria-hidden="true">&rarr;</span>
