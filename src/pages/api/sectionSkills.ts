d91199dc5a5b53cc89247f51e25797713b075269
Lucas Trindade <trindadebra@gmail.com>
2025-03-02 22:37:43 -0300

    fix: clean tags

--- src/pages/api/sectionSkills.ts
+++ src/pages/api/sectionSkills.ts
@@ -12,9 +12,11 @@ export async function getSectionSkills(): Promise<Skill[]> {
     database_id: "f956ac4be74a42f8a9171149c1c9bc5a",
   });
 
-  return response.results.map((skill: any) => ({
+  const skills = response.results.map((skill: any) => ({
     skill_name: skill.properties['skill_name'].title[0]?.text.content,
   }));
+
+  return skills.filter(skill => skill.skill_name);
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
