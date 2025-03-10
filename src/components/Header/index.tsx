350266794daddea8f21cd78f6e33daee71037a12
Lucas Trindade <trindadebra@gmail.com>
2025-03-10 05:34:17 -0300

    fix: build fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -68,7 +68,7 @@ export default function Example() {
 
           </div>
         </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-10" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
             <div className="flex items-center justify-between">
